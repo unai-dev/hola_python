ffbbf4ab7d67a5f6846d507f63b2fd25f23228c8
unaidev <unaidevcoding@gmail.com>
2025-05-27 19:10:42 +0200

    Actualiza ejemplos de variables en 01_variables.py

--- python_basic/00_hola_mundo.py
+++ python_basic/00_hola_mundo.py
@@ -1,2 +1,19 @@
-# Hola Mundo
-print("Hola Mundo")
+### Hola Mundo ###
+
+# Nuestro hola mundo en Python
+print("Hola Python")
+print('Hola Python')
+
+# Esto es un comentario
+
+"""
+Este es un
+comentario
+en varias líneas
+"""
+
+'''
+Este también es un
+comentario
+en varias líneas
+'''
